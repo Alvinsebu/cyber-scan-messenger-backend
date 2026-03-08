f7bd81ea5203dcb863309e16ee068237bc0fff5a
alvin <alvinsebastian779@gmail.com>
2026-03-08 11:00:23 +0530

    updated

---
+++ clear_database.py
@@ -0,0 +1,11 @@
+from app import create_app
+from app.models import db
+
+app = create_app()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    print("Database reset successfully")
+
